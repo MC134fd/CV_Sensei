f71c68231caec3d138148a981283ae88a95ce0ec
Wiktoria Sudak wiktoria.sudak@icloud.com <wiktoria.sudak@icloud.com>
2025-11-24 21:29:02 +0000

    Finished devise setup with navbar partial and sign up & sign in pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # Makes sure the user is logged in before allowing them to see any page
+  # (apart from the homepage - set in pages controller)
 end
