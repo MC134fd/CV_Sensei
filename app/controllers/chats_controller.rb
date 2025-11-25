982595bea5305b2e6d61caba5f5215870b654eaa
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-25 14:07:46 +0000

    Add more stuff

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,22 @@
 class ChatsController < ApplicationController
   def create
     @chat = Chat.new(chat_params)
-    @chat.cv = params[:cv_id]
+    @chat.cv = Cv.find(params[:cv_id])
     if @chat.save
-      redirect_to @chat, status: :see_other
+      # redirect_to cv_chat_path(@chat.cv, @chat), status: :see_other
+      @chats = @chat.cv.chats
+      render 'cvs/show', status
     else
-      render :new
-      render 'rooms/show', status: :unprocessable_entity
+      # # render :new
+      # render 'cvs/show', status: :unprocessable_entity
+
+      # # Ensure rooms/show has what it needs
+      # @chats = @room.chats.order(created_at: :asc)
+      # render 'rooms/show', status: :unprocessable_entity
+
+      @chats = @chat.cv.chats
+      render 'cvs/show', status
+
     end
   end
 
