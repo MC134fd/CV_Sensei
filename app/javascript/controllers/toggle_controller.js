369793c13187555d6265ad465682c550041a58ca
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-26 14:08:50 +0000

    Removed console logs

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,19 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  connect() {
-    console.log("connected");
-  }
-
   static targets = ["openSidebar", "sidebar"];
 
   call(event) {
-    console.log("running toggle#call");
     event.preventDefault();
-    console.log(this.element);
-    console.log(this.openSidebarTarget)
-
-
     this.sidebarTarget.classList.toggle("d-none")
     this.openSidebarTarget.classList.toggle("d-none")
   }
