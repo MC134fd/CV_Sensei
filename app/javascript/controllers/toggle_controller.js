c8ca360d517017a0ca8be62ac432af141480ae8b
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-27 11:07:15 +0000

    Changed stimulus sidebar to open and close actions

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -10,4 +10,20 @@ export default class extends Controller {
     this.openSidebarTarget.classList.toggle("d-none");
     this.element.classList.toggle("border-end");
   }
+
+  open(event) {
+    event.preventDefault();
+    this.openSidebarTarget.classList.add("d-none");
+    this.sidebarTarget.classList.remove("d-none");
+    this.element.classList.remove("col-md-1");
+    this.element.classList.add("border-end col-md-2");
+
+  }
+  close(event) {
+    event.preventDefault();
+    this.openSidebarTarget.classList.remove("d-none");
+    this.sidebarTarget.classList.add("d-none");
+    this.element.classList.remove("border-end col-md-2");
+    this.element.classList.add("col-md-1")
+  }
 }
