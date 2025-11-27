7cbc71c3143ac6f0ad8a133df47a6f053ed093a6
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-27 11:41:31 +0000

    split sidebar stimulus controller into openSidebar and closeSidebar functions

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,28 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["openSidebar", "sidebar"];
+  static targets = ["sidebarCol", "contentCol", "openSidebarButton", "sidebar"];
 
-  call(event) {
+  openSidebar(event) {
     event.preventDefault();
-    this.sidebarTarget.classList.toggle("d-none");
-    this.openSidebarTarget.classList.toggle("d-none");
-    this.element.classList.toggle("border-end");
-  }
-
-  open(event) {
-    event.preventDefault();
-    this.openSidebarTarget.classList.add("d-none");
+    this.sidebarColTarget.classList.remove("col-md-1");
+    this.sidebarColTarget.classList.add("col-md-2", "border-end");
+    this.contentColTarget.classList.remove("col-md-11");
+    this.contentColTarget.classList.add("col-md-10");
+    this.openSidebarButtonTarget.classList.add("d-none");
     this.sidebarTarget.classList.remove("d-none");
-    this.element.classList.remove("col-md-1");
-    this.element.classList.add("border-end col-md-2");
-
   }
-  close(event) {
+  closeSidebar(event) {
     event.preventDefault();
-    this.openSidebarTarget.classList.remove("d-none");
+    this.sidebarColTarget.classList.remove("col-md-2", "border-end");
+    this.sidebarColTarget.classList.add("col-md-1");
+    this.contentColTarget.classList.remove("col-md-10");
+    this.contentColTarget.classList.add("col-md-11");
+    this.openSidebarButtonTarget.classList.remove("d-none");
     this.sidebarTarget.classList.add("d-none");
-    this.element.classList.remove("border-end col-md-2");
-    this.element.classList.add("col-md-1")
   }
 }
