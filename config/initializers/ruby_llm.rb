9fe3c3964c66f1c369644990a5422652392d4133
Zainab Saigol <z.s.saigol@hotmail.com>
2025-11-28 14:40:03 +0000

    added comments to the rubyllm config file for team clarity

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,7 +1,13 @@
+# DO NOT REMOVE ANY LINES FROM THIS FILE!!!
+# Un-comment the config you need.
+# Comment out the other config.
+
+# config for PAID open ai key:
 RubyLLM.configure do |config|
   config.openai_api_key = ENV["OPENAI_API_KEY"]
 end
 
+# config for FREE github key:
 # RubyLLM.configure do |config|
 #   config.openai_api_key = ENV["GITHUB_TOKEN"]
 #   config.openai_api_base = "https://models.inference.ai.azure.com"
